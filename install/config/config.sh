d7a21801fab24258dd4549304e501178646f5677
capnbiggin <capnbiggin@proton.me>
2025-08-15 21:33:50 -0500

    automatic commit

--- install/config/config.sh
+++ install/config/config.sh
@@ -5,6 +5,12 @@ mkdir -p ~/.config/zsh/plugins/
 
 # Link capndot configs
 ln -s ~/.local/share/capndot/config/* ~/.config/
+
+# Link zsh Configs
 ln -s ~/.local/share/capndot/default/shell/zshenv ~/.zshenv
 ln -s ~/.local/share/capndot/default/shell/zshrc ~/.config/zsh/.zshrc
 ln -s ~/.local/share/capndot/default/shell/zprofile ~/.config/zsh/.zprofile
+
+# copy over bash Configs
+cp -r ~/.local/share/capndot/default/shell/bashrc ~/.bashrc
+cp -r ~/.local/share/capndot/default/shell/bash_profile ~/.bash_profile
